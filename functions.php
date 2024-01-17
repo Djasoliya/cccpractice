ed313c041826f979fab6764788bba1239ab225d1
Djasoliya <dhruvitjasoliya1818@gmail.com>
2024-01-17 16:13:58 +0530

    basic function

---
+++ functions.php
@@ -0,0 +1,37 @@
+<?php
+// $str1 = "dhruvit";
+// echo strlen($str1);
+
+// $str2 = "hello world";
+// echo  str_replace("hello", "hii", $str2);
+
+// $str3 = "string position";
+// echo strpos($str3, 'r');
+
+// $str4 = "find the substring";
+// echo substr($str4, 5, 3);
+
+// $str5 = "CONVERT THE STRING LOWERCASE";
+// echo strtolower($str5);
+
+// $str6 = "converts a string to uppercase";
+// echo strtoupper($str6);
+
+// $str7 = "            Removes whitespace or other predefined characters from the beginning and end of a string";
+// echo trim($str7);
+
+// $str8 = array("joins", "array", "elements", "with", "string");
+// echo implode("/", $str8);
+// echo implode("-", $str8);
+
+// $str9 = "Splits a string into an array by a specified delimiter";
+// $a = explode(" ", $str9);
+// echo $a[2]."<br>";
+// $b = explode("a", $str9);
+// print_r($b);
+
+$str10 = "converts special characters to HTML entities";
+echo htmlspecialchars($string);
+
+
+?>
