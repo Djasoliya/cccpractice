549d7d0c7347898827c7e537bb3c882eefb80790
Djasoliya <dhruvitjasoliya1818@gmail.com>
2024-01-19 11:41:37 +0530

    upadte exercise

--- string_exercise_1.php
+++ string_exercise_1.php
@@ -1,4 +1,10 @@
 <?php
+
+// input
+// 	$firstname = “firstname” (Your first name in small letter)
+// 	$middlename = “middlename” (Your father name name in small letter)
+// 	$surname = “surname” (Your surname in small letter)
+
 $firstname = "dhruvit";
 $middlename = "kanjibhai";
 $surname = "jasoliya";
--- string_exercise_5.php
+++ string_exercise_5.php
@@ -2,12 +2,15 @@
 $quote = "In three words I can sum up everything I've learned about life: it goes on.";
 
 // 1. Count the total number of words in the quote.
-echo str_word_count($quote)."<br>";
+echo "1. Count the total number of words in the quote.<br>";
+echo str_word_count($quote)."<br><br>";
 
 // 2. Convert the entire quote to lowercase.
-echo strtolower($quote)."<br>";
+echo "2. Convert the entire quote to lowercase.<br>";
+echo strtolower($quote)."<br><br>";
 
 // 3. Capitalize the first letter of each word in the quote.
+echo "3. Capitalize the first letter of each word in the quote.<br>";
 echo ucwords($quote);
 
 
